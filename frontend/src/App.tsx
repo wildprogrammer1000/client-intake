de72ab28a2b9e7a63d1368309a84b846858a451b
wildprogrammer1000 <wildprogrammer1000@gmail.com>
2026-05-06 08:43:55 +0900

    feat: 페이지 이동시 스크롤 초기화

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom'
 import AdminPage from './AdminPage'
 import HomePage from './pages/HomePage'
 import FeatureExtensionInquiryPage from './pages/inquiry/FeatureExtensionInquiryPage'
 import IssueResolutionInquiryPage from './pages/inquiry/IssueResolutionInquiryPage'
 import NewServiceInquiryPage from './pages/inquiry/NewServiceInquiryPage'
 
+function ScrollToTopOnNavigate() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  return null
+}
+
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTopOnNavigate />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/inquiry/new-service" element={<NewServiceInquiryPage />} />
